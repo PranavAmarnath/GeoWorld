1362bc573b9a98ef2c990d8dbad82140749bdedc
PranavAmarnath <64337291+PranavAmarnath@users.noreply.github.com>
2021-08-05 12:28:47 -0700

    Added MapsForge maps with nav drawer

--- app/src/main/java/com/secres/geoworld/MainActivity.java
+++ app/src/main/java/com/secres/geoworld/MainActivity.java
@@ -1,16 +1,46 @@
 package com.secres.geoworld;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.datastore.MapDataStore;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.download.TileDownloadLayer;
+import org.mapsforge.map.layer.download.tilesource.OpenStreetMapMapnik;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import java.io.FileInputStream;
+import java.util.Objects;
 
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.WorldWindow;
@@ -23,16 +53,95 @@ import gov.nasa.worldwind.layer.RenderableLayer;
 import gov.nasa.worldwind.render.Color;
 import gov.nasa.worldwind.shape.Placemark;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Placemark ventura;
-    WorldWindow wwd;
+    private Placemark placemark;
+    private WorldWindow wwd;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private MapView mapView;
+    private static final int SELECT_MAP_FILE = 0;
+    private double latitude = 37.336316657274814;
+    private double longitude = -122.06142995877659;
+    private double altitude = 0;
+    private LookAt lookAt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        setNavigationViewListener();
+
+        AndroidGraphicFactory.createInstance(getApplication());
+    }
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.nav_home) {
+            ViewGroup layoutToChange = findViewById(R.id.home);
+            layoutToChange.removeAllViews();
+
+            LayoutInflater inflater = LayoutInflater.from(this);
+            RelativeLayout newLayout = (RelativeLayout) inflater.inflate(R.layout.home_layout, null);
+
+            layoutToChange.addView(newLayout);
+        }
+        else if(id == R.id.nav_satellite) {
+            ViewGroup layoutToChange = findViewById(R.id.home);
+            layoutToChange.removeAllViews();
+
+            LayoutInflater inflater = LayoutInflater.from(this);
+            RelativeLayout newLayout = (RelativeLayout) inflater.inflate(R.layout.satellite_layout, null);
+
+            layoutToChange.addView(newLayout);
+
+            createSatelliteView();
+        }
+        else if(id == R.id.nav_map) {
+            ViewGroup layoutToChange = findViewById(R.id.home);
+            layoutToChange.removeAllViews();
+
+            LayoutInflater inflater = LayoutInflater.from(this);
+            RelativeLayout newLayout = (RelativeLayout) inflater.inflate(R.layout.map_layout, null);
+
+            layoutToChange.addView(newLayout);
+            createMapView();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void createSatelliteView() {
         // Create a WorldWindow (a GLSurfaceView)...
         wwd = new WorldWindow(getApplicationContext());
         // ... and add some map layers
@@ -41,30 +150,30 @@ public class MainActivity extends AppCompatActivity {
 
         RenderableLayer placemarksLayer = new RenderableLayer("Placemarks");
         wwd.getLayers().addLayer(placemarksLayer);
-        ventura = Placemark.createWithColorAndSize(Position.fromDegrees(37.336316657274814, -122.06142995877659, 0), new Color(0, 1, 1, 1), 20);
-        placemarksLayer.addRenderable(ventura);
+        placemark = Placemark.createWithColorAndSize(Position.fromDegrees(latitude, longitude, altitude), new Color(0, 1, 1, 1), 20);
+        placemarksLayer.addRenderable(placemark);
 
         adjustView();
 
-        FloatingActionButton mapFab = findViewById(R.id.map_fab);
+        FloatingActionButton mapFab = findViewById(R.id.globe_fab);
+        System.out.println(mapFab);
         mapFab.setOnClickListener(view -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setMessage("Enter coordinates:");
 
-            // set the custom layout
             final View customLayout = getLayoutInflater().inflate(R.layout.dialog_layout, null);
             builder.setView(customLayout);
 
             builder.setPositiveButton("OK", (dialog, id) -> {
                 EditText latitudeText = customLayout.findViewById(R.id.latitudeText);
-                double latitudeDegrees = Double.parseDouble(latitudeText.getText().toString());
+                latitude = Double.parseDouble(latitudeText.getText().toString());
 
                 EditText longitudeText = customLayout.findViewById(R.id.longitudeText);
-                double longitudeDegrees = Double.parseDouble(longitudeText.getText().toString());
+                longitude = Double.parseDouble(longitudeText.getText().toString());
 
-                placemarksLayer.removeRenderable(ventura);
-                ventura = Placemark.createWithColorAndSize(Position.fromDegrees(latitudeDegrees, longitudeDegrees, 0), new Color(0, 1, 1, 1), 20);
-                placemarksLayer.addRenderable(ventura);
+                placemarksLayer.removeRenderable(placemark);
+                placemark = Placemark.createWithColorAndSize(Position.fromDegrees(latitude, longitude, altitude), new Color(0, 1, 1, 1), 20);
+                placemarksLayer.addRenderable(placemark);
 
                 adjustView();
 
@@ -80,15 +189,65 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Add the WorldWindow view object to the layout that was reserved for the globe.
-        FrameLayout globeLayout = (FrameLayout) findViewById(R.id.globe);
+        FrameLayout globeLayout = findViewById(R.id.globe);
         globeLayout.addView(wwd);
     }
 
     private void adjustView() {
-        Position pos = ventura.getPosition();
-        LookAt lookAt = new LookAt().set(pos.latitude, pos.longitude, pos.altitude, WorldWind.ABSOLUTE,
+        Position pos = placemark.getPosition();
+        lookAt = new LookAt().set(pos.latitude, pos.longitude, pos.altitude, WorldWind.ABSOLUTE,
                 1e7, 0, 0, 0);
         wwd.getNavigator().setAsLookAt(wwd.getGlobe(), lookAt);
+
+        altitude = pos.altitude;
+    }
+
+    private void createMapView() {
+        mapView = new MapView(getApplicationContext());
+
+        Intent intent = new Intent(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ? Intent.ACTION_OPEN_DOCUMENT : Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, SELECT_MAP_FILE);
+
+        // Add the MapView view object to the layout that was reserved for the map.
+        FrameLayout globeLayout = findViewById(R.id.map);
+        globeLayout.addView(mapView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_MAP_FILE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Uri uri = data.getData();
+                openMap(uri);
+            }
+        }
+    }
+
+    private void openMap(Uri uri) {
+        try {
+            mapView.getMapScaleBar().setVisible(true);
+            mapView.setBuiltInZoomControls(true);
+
+            TileCache tileCache = AndroidUtil.createTileCache(this, "mapcache",
+                    mapView.getModel().displayModel.getTileSize(), 1f,
+                    mapView.getModel().frameBufferModel.getOverdrawFactor());
+
+            FileInputStream fis = (FileInputStream) getContentResolver().openInputStream(uri);
+            MapDataStore mapDataStore = new MapFile(fis);
+            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
+                    mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
+            tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
+
+            mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+            mapView.setCenter(new LatLong(52.517037, 13.38886));
+            mapView.setZoomLevel((byte) 12);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
